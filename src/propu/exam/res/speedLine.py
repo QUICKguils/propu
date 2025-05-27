716ad70e57e72fe964ed504b522177269fb01b4f
QUICKguils <guilainsjc@gmail.com>
2025-05-27 08:42:10 +0200

    exam: Add resource files

---
+++ src/propu/exam/res/speedLine.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from numpy import *
+import pandas
+import scipy
+
+# ------------------------------------------------------------------------------
+# read all available characteristic data
+# ------------------------------------------------------------------------------
+speeds = [90,95,100]
+perfo = {}
+for s in speeds:
+    fname = "speedLine_%sNn.csv"%s
+    perfo[s] = pandas.read_csv(fname,dtype=float64,
+                               header=None,names=["mFlow","pRatio","efficiency"])
+
+# ------------------------------------------------------------------------------
+# interpolate (pressure ratio, polytropic efficiency) ifo mass flow on characteristic
+# - perfo : characteristic
+# - mFlow : fraction of nominal corrected mass flow rate
+# ------------------------------------------------------------------------------
+def performance1(perfoNn,mFlow):
+    p = interp(mFlow,perfoNn["mFlow"],perfoNn["pRatio"])
+    e = interp(mFlow,perfoNn["mFlow"],perfoNn["efficiency"])
+    return (p,e) 
+
+# ------------------------------------------------------------------------------
+# find (pressure ratio, polytropic efficiency) ifo speed and mass flow
+# - speed : percentage (integer) of nominal corrected rotation speed
+# - mFlow : fraction of nominal corrected mass flow rate 
+# ------------------------------------------------------------------------------
+def performance(speed,mFlow): return performance1(perfo[speed],mFlow)
